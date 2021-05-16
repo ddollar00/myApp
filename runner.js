f2516ab3d86ed16d800456342ccf6cc14f18ca8d
Damion  Dollar <dollardamion@gmail.com>
2021-05-16 13:51:04 -0400

    numba 2

--- runner.js
+++ runner.js
@@ -1,24 +1,2 @@
 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       
-        <p>
-          Edit <code>src/App.js,  Dame's first edit , Let's GO</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React is cool
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+console.log('hello');
